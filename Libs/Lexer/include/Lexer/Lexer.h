b51e44069b184988e02679e52c74b8e88085f6c6
Zofikar <pawel.chwalczyk@gmail.com>
2025-08-24 18:30:36 +0200

    Add clang-tidy config and format files

--- Libs/Lexer/include/Lexer/Lexer.h
+++ Libs/Lexer/include/Lexer/Lexer.h
@@ -3,25 +3,27 @@
 
 #include <vector>
 
-namespace Lexer {
+namespace Lexer
+{
 
-class Lexer {
-public:
-  explicit Lexer(std::string source, std::string filename = "<memory>");
+    class Lexer
+    {
+    public:
+        explicit Lexer(std::string source, std::string filename = "<memory>");
 
-  Token next();
-  Token peek(std::size_t lookahead = 0);
-  std::vector<Token> tokenize();
+        Token next();
+        Token peek(std::size_t lookahead = 0);
+        std::vector<Token> tokenize();
 
-private:
-  std::string source;
-  std::string filename;
-  std::size_t pos = 0;
+    private:
+        std::string source;
+        std::string filename;
+        std::size_t pos = 0;
 
-  Token makeToken(TokenKind kind, std::size_t start, std::size_t length) const;
-  [[nodiscard]] char peekChar(std::size_t offset = 0) const;
-  char advance();
-  [[nodiscard]] bool eof() const;
-};
+        Token makeToken(TokenKind kind, std::size_t start, std::size_t length) const;
+        [[nodiscard]] char peekChar(std::size_t offset = 0) const;
+        char advance();
+        [[nodiscard]] bool eof() const;
+    };
 
 } // namespace Lexer
--- Libs/Lexer/test/main.cpp
+++ Libs/Lexer/test/main.cpp
@@ -1,21 +1,24 @@
-#include <gtest/gtest.h>
 #include <Lexer/Lexer.h>
 
-TEST(LexerIdentifiers, SimpleIdents) {
-  Lexer::Lexer lx("foo bar123");
-  auto toks = lx.tokenize();
+#include <gtest/gtest.h>
 
-  ASSERT_EQ(toks[0].kind, Lexer::TokenKind::Ident);
-  EXPECT_EQ(toks[0].lexeme, "foo");
+TEST(LexerIdentifiers, SimpleIdents)
+{
+    Lexer::Lexer lx("foo bar123");
+    auto toks = lx.tokenize();
 
-  ASSERT_EQ(toks[1].kind, Lexer::TokenKind::Ident);
-  EXPECT_EQ(toks[1].lexeme, "bar123");
+    ASSERT_EQ(toks[0].kind, Lexer::TokenKind::Ident);
+    EXPECT_EQ(toks[0].lexeme, "foo");
+
+    ASSERT_EQ(toks[1].kind, Lexer::TokenKind::Ident);
+    EXPECT_EQ(toks[1].lexeme, "bar123");
 }
 
-TEST(LexerIdentifiers, UnderscoreIdents) {
-  Lexer::Lexer lx("_baz");
-  auto toks = lx.tokenize();
+TEST(LexerIdentifiers, UnderscoreIdents)
+{
+    Lexer::Lexer lx("_baz");
+    auto toks = lx.tokenize();
 
-  ASSERT_EQ(toks[0].kind, Lexer::TokenKind::Ident);
-  EXPECT_EQ(toks[0].lexeme, "_baz");
+    ASSERT_EQ(toks[0].kind, Lexer::TokenKind::Ident);
+    EXPECT_EQ(toks[0].lexeme, "_baz");
 }
